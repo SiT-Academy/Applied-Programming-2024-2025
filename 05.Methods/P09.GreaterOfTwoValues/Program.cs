3d4a2a743d4e906986c26491ebf4ab76fc46d595
Deniz Ivanov <d.ivan0v6100@gmail.com>
2025-02-28 20:26:46 +0200

    Added exercises after lecture.

---
+++ 05.Methods/P09.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace P09.GreaterOfTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string par1 = Console.ReadLine();
+            string par2 = Console.ReadLine();
+
+            string result = string.Empty;
+            if (type == "int")
+            {
+               result = GetMax(int.Parse(par1), int.Parse(par2));
+            }
+            else if (type == "char")
+            {
+                result = GetMax(char.Parse(par1), char.Parse(par2));
+            }
+            else if(type == "string")
+            {
+                result = GetMax(par1, par2);
+            }
+
+            Console.WriteLine(result);
+        }
+        static string GetMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a.ToString();
+            }
+            return b.ToString();
+        }
+        static string GetMax(char a, char b)
+        {
+            if (a > b)
+            {
+                return a.ToString();
+            }
+            return b.ToString();
+        }
+        static string GetMax(string a, string b)
+        {
+            int result = a.CompareTo(b);
+            if (result > 0)
+            {
+                return a;
+            }
+            if(result < 0)
+            {
+                return b;
+            }
+            return string.Empty;
+        }
+        
+    }
+}
---
+++ 05.Methods/P10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P10.MultiplyEvensByOdds
+{
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Math.Abs(int.Parse(Console.ReadLine()));
+            string numberToString = input.ToString();
+            int[] numbersArray = numberToString
+                .Select(digit => int.Parse(digit.ToString())).ToArray();
+            int result = MultiplyEvensByOdds(numbersArray);
+
+            Console.WriteLine(result);
+        }
+        static int MultiplyEvensByOdds(int[] numbers)
+        {
+            int oddSum = 0;
+            int evenSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currNumber = numbers[i];
+                if (currNumber % 2 == 0)
+                {
+                    evenSum += currNumber;
+                }
+                else
+                {
+                    oddSum += currNumber;
+                }
+            }
+            return evenSum * oddSum;
+        }
+    }
+}
